0287bb86b4172ca903547d9343857ff0dc0ccdbd
Long Hoang <long@mindworker.de>
2017-03-19 11:58:55 +0800

    Implement all action requests

--- public/script.js
+++ public/script.js
@@ -1,20 +1,39 @@
 var app = angular.module('app', []);
 
 app.controller('formCtrl', function($scope, $window, $http) {
-  $scope.width = $window.innerWidth;
-  $scope.height = $window.innerHeight;
+  $scope.websiteUrl = window.location.href
+  $scope.windowSize = {
+    width: $window.innerWidth,
+    height: $window.innerHeight
+  }
 
-  $http.post("/sessions")
-       .then(function(resp){
-	 $scope.sessionID = resp.data.session;
-       }, function(err) {
-	 // @TODO handle error gracefully
-	 alert(err);
-       });
+  $http.post("/sessions").then(function(resp){
+    $scope.session = resp.data.session;
+    console.log($scope.session)
+    $scope.active = true;
+  }, function(err) {
+    $scope.error = "Error on getting valid session. Is the backend running? Is this file served by the backend?";
+    console.error(err);
+  });
 
+  $scope.sendAction = function(data) {
+    var body = angular.extend({
+      sessionId: $scope.session,
+      websiteUrl: $scope.websiteUrl
+    }, data);
+
+    $http.post("/actions", body).then(function(){}, function(err) {
+      $scope.error = "Send action of " + data.eventType + "failed";
+      console.error(err);
+    });
+  }
   
   $scope.paste = function(event) {
-    console.log(event.target.id);
+    $scope.sendAction({
+      eventType: "copyAndPaste",
+      pasted: true,
+      formId: event.target.id
+    });
   }
 
   $scope.key = function(event) {
@@ -35,6 +54,13 @@ app.controller('formCtrl', function($scope, $window, $http) {
   $scope.submit = function() {
     var timeTaken = Math.ceil(((new Date())-$scope.timeStarted)/1000);
     console.log("Time taken:", timeTaken);
+
+    $scope.sendAction({
+      eventType: "timeTaken",
+      time: timeTaken
+    });
+
+    $scope.active = false;
   }
 
 });
@@ -43,11 +69,18 @@ app.directive('resize', ['$window', function ($window) {
   function link(scope, element, attrs){
     angular.element($window).bind('resize', function(){
 
-      var width = $window.innerWidth;
-      var height = $window.innerHeight;
+      var newSize = {
+	width: $window.innerWidth,
+	height: $window.innerHeight
+      }
+      console.log("old:", scope.windowSize);
+      console.log("new:", newSize);
 
-      console.log("old:", scope.width, scope.height)
-      console.log("new:", width, height);
+      scope.sendAction({
+	eventType: "resizeWindow",
+	resizeFrom: scope.windowSize,
+	resizeTo: newSize
+      });
 
       // manuall $digest required as resize event
       // is outside of angular
